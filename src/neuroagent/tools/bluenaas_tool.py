fad474f78103583c4f1665680c0f48dad442c7d9
Jan <18519371+jankrepl@users.noreply.github.com>
2024-10-07 16:20:20 +0200

    Finalize POC

--- src/neuroagent/tools/bluenaas_tool.py
+++ src/neuroagent/tools/bluenaas_tool.py
@@ -1,4 +1,5 @@
 """BlueNaaS single cell stimulation, simulation and synapse placement tool."""
+
 from typing import Literal, Annotated
 from pydantic import BaseModel, Field, PositiveInt
 
@@ -9,34 +10,47 @@ from neuroagent.utils import get_kg_data
 
 logger = logging.getLogger(__name__)
 
+
 class SimulationStimulusConfig(BaseModel):
-    stimulusType: Literal["current_clamp", "voltage_clamp", "conductance"] = Field(
-        description="Type of stimulus to be used."
+    stimulus_type: Literal["current_clamp", "voltage_clamp", "conductance"] = Field(
+        default="current_clamp", description="Type of stimulus to be used."
     )
-    stimulusProtocol: Literal["ap_waveform", "idrest", "iv", "fire_pattern"] = Field(
-        description="Stimulus protocol to be used."
+    stimulus_protocol: Literal["ap_waveform", "idrest", "iv", "fire_pattern"] = Field(
+        default="ap_waveform", description="Stimulus protocol to be used."
     )
 
-    amplitudes: list[float]  = Field(default_factory=list, min_items=1, description="List of amplitudes for the stimulus")
+    amplitudes: list[float] = Field(
+        default_factory=lambda: [0.1],
+        min_items=1,
+        description="List of amplitudes for the stimulus",
+    )
+
 
 class CurrentInjectionConfig(BaseModel):
-    injectTo: str = Field(
-        description="Section to inject the current to."
+    inject_to: str = Field(
+        default="soma[0]", description="Section to inject the current to."
     )
-    stimulus: SimulationStimulusConfig
+    stimulus: SimulationStimulusConfig = SimulationStimulusConfig()
+
 
 class RecordingLocation(BaseModel):
-    section: str #TODO: how to constrain this, we need to query available section ids from the obtained me model's morphology
-    offset: float = Field(ge=0, le=1, description="Offset in the section to record from")
+    section: str = Field(default="soma[0]", description="Section to record from")
+    offset: float = Field(
+        default=0.5, ge=0, le=1, description="Offset in the section to record from"
+    )
+
 
 class SimulationConditionsConfig(BaseModel):
     celsius: int = Field(default=34, ge=0, le=50, description="Temperature in celsius")
     vinit: int = Field(default=-73, description="Initial voltage in mV")
     hypamp: int = Field(default=0, description="Holding current in nA")
-    max_time: int = Field(default=100, le=3000, description="Maximum simulation time in ms")
-    time_step: float = Field(default=0.05, ge=0.001, le=10, description="Time step in ms")
+    max_time: int = Field(
+        default=100, le=3000, description="Maximum simulation time in ms"
+    )
+    time_step: float = Field(
+        default=0.05, ge=0.001, le=10, description="Time step in ms"
+    )
     seed: int = Field(default=100, description="Random seed")
-
 
 
 class InputBlueNaaS(BaseModel):
@@ -49,33 +63,75 @@ class InputBlueNaaS(BaseModel):
         )
     )
 
-    currentInjection: CurrentInjectionConfig = Field(
+    current_injection: CurrentInjectionConfig = Field(
+        default=CurrentInjectionConfig(),
         description=(
             "Configuration for current injection. Includes the target section to inject"
             " to and the stimulus configuration."
-        )
+        ),
     )
-    recordFrom: list[RecordingLocation] = Field(
+    record_from: list[RecordingLocation] = Field(
+        default_factory=lambda: [RecordingLocation()],
         description=(
             "List of sections to record from during the simulation. Each record"
             " configuration includes the section name and offset."
-        )
+        ),
     )
     conditions: SimulationConditionsConfig = Field(
+        default=SimulationConditionsConfig(),
         description=(
             "Simulation conditions including temperature (celsius), initial voltage"
             " (vinit, in mV), hyperpolarizing current (hypamp, in nA), maximum simulation time"
             " (max_time, in ms), time step (time_step, in ms), and random seed (seed)."
-        )
-    )
-    simulationType: Literal["single-neuron-simulation"] = Field(
-        description=(
-            "Type of the simulation. For now , its set to only single-neuron-simulation"
-        )
-    )
-    simulationDuration: int = Field(
-        description=(
-            "Duration of the simulation in milliseconds."
-        )
+        ),
     )
 
+
+class BlueNaaSOutput(BaseToolOutput):
+    """Should return a successful POST request"""
+
+    status: Literal["success", "pending", "error"]
+
+
+class BlueNaaSTool(BasicTool):
+    name: str = "bluenaas-tool"
+    description: str = """Runs a single-neuron simulation using the BlueNaaS service.
+    Requires a "me_model_id" which must be fetched by get-me-model-tool.
+
+    The tool 
+    """
+    metadata: dict[str, Any]
+    args_schema: Type[BaseModel] = InputBlueNaaS
+
+    def _run(self) -> None:
+        pass
+
+    async def _arun(
+        self,
+        me_model_id: str,
+        current_injection: CurrentInjectionConfig,
+        record_from: list[RecordingLocation],
+        conditions: SimulationConditionsConfig,
+    ) -> BaseToolOutput:
+        """Run the BlueNaaS tool."""
+        logger.info(f"Running BlueNaaS tool")
+
+        try:
+            response = await self.metadata["httpx_client"].post(
+                url=self.metadata["url"],
+                params={"model_id": me_model_id},
+                headers={"Authorization": f"Bearer {self.metadata["token"]}"},
+                json={
+                    "currentInjection": current_injection.model_dump(),
+                    "recordFrom": [rec.model_dump() for rec in recordFrom],
+                    "conditions": conditions.model_dump(),
+                    "simulationType": "single-neuron-simulation",
+                    "simulationDuration": conditions.max_time,
+                },
+                timeout=5.0,
+            )
+
+            return BlueNaaSOutput(status="success")
+
+        except Exception as e:
+            raise ToolException(str(e), self.name)
