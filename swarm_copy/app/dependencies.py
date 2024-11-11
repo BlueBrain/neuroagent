03f1104aa53bbfed0b545694ad15e53678347236
Boris Bergsma <bergsma.boris@gmail.com>
2024-11-11 13:03:52 +0100

    add Vlab / project validation

--- swarm_copy/app/dependencies.py
+++ swarm_copy/app/dependencies.py
@@ -7,11 +7,15 @@ from typing import Annotated, Any, AsyncIterator
 from fastapi import Depends, HTTPException, Request
 from fastapi.security import HTTPBearer
 from httpx import AsyncClient, HTTPStatusError
+from keycloak import KeycloakOpenID
 from openai import AsyncOpenAI
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from starlette.status import HTTP_401_UNAUTHORIZED
 
+from swarm_copy.app.app_utils import validate_project
 from swarm_copy.app.config import Settings
+from swarm_copy.app.database.sql_schemas import Threads
 from swarm_copy.new_types import Agent
 from swarm_copy.run import AgentsRoutine
 from swarm_copy.tools import PrintAccountDetailsTool
@@ -107,22 +111,6 @@ async def get_session(
         yield session
 
 
-def get_starting_agent(
-    settings: Annotated[Settings, Depends(get_settings)],
-) -> Agent:
-    """Get the starting agent."""
-    logger.info(f"Loading model {settings.openai.model}.")
-    agent = Agent(
-        name="Agent",
-        instructions="""You are a helpful assistant helping scientists with neuro-scientific questions.
-                You must always specify in your answers from which brain regions the information is extracted.
-                Do no blindly repeat the brain region requested by the user, use the output of the tools instead.""",
-        tools=[PrintAccountDetailsTool],
-        model=settings.openai.model,
-    )
-    return agent
-
-
 async def get_user_id(
     token: Annotated[str, Depends(auth)],
     settings: Annotated[Settings, Depends(get_settings)],
@@ -149,12 +137,78 @@ async def get_user_id(
         return "dev"
 
 
-def get_context_variables(
+def get_kg_token(
     settings: Annotated[Settings, Depends(get_settings)],
-    starting_agent: Annotated[Agent, Depends(get_starting_agent)],
-) -> dict[str, Any]:
-    """Get the global context variables to feed the tool's metadata."""
-    return {"user_id": 1234, "starting_agent": starting_agent}
+    token: Annotated[str | None, Depends(auth)],
+) -> str:
+    """Get a Knowledge graph token using Keycloak."""
+    if token:
+        return token
+    else:
+        instance = KeycloakOpenID(
+            server_url=settings.keycloak.server_url,
+            realm_name=settings.keycloak.realm,
+            client_id=settings.keycloak.client_id,
+        )
+        return instance.token(
+            username=settings.keycloak.username,
+            password=settings.keycloak.password.get_secret_value(),  # type: ignore
+        )["access_token"]
+
+
+async def get_vlab_and_project(
+    user_id: Annotated[str, Depends(get_user_id)],
+    session: Annotated[AsyncSession, Depends(get_session)],
+    request: Request,
+    settings: Annotated[Settings, Depends(get_settings)],
+    httpx_client: Annotated[AsyncClient, Depends(get_httpx_client)],
+    token: Annotated[str, Depends(get_kg_token)],
+) -> dict[str, str]:
+    """Get the current vlab and project ID."""
+    if "x-project-id" in request.headers and "x-virtual-lab-id" in request.headers:
+        vlab_and_project = {
+            "vlab_id": request.headers["x-virtual-lab-id"],
+            "project_id": request.headers["x-project-id"],
+        }
+    elif not settings.keycloak.validate_token:
+        vlab_and_project = {
+            "vlab_id": "430108e9-a81d-4b13-b7b6-afca00195908",
+            "project_id": "eff09ea1-be16-47f0-91b6-52a3ea3ee575",
+        }
+    else:
+        thread_id = request.path_params.get("thread_id")
+        thread_result = await session.execute(
+            select(Threads).where(
+                Threads.user_id == user_id, Threads.thread_id == thread_id
+            )
+        )
+        thread = thread_result.scalars().one_or_none()
+        if not thread:
+            raise HTTPException(
+                status_code=404,
+                detail={
+                    "detail": "Thread not found.",
+                },
+            )
+        if thread and thread.vlab_id and thread.project_id:
+            vlab_and_project = {
+                "vlab_id": thread.vlab_id,
+                "project_id": thread.project_id,
+            }
+        else:
+            raise HTTPException(
+                status_code=404,
+                detail="thread not found when trying to validate project ID.",
+            )
+
+    await validate_project(
+        httpx_client=httpx_client,
+        vlab_id=vlab_and_project["vlab_id"],
+        project_id=vlab_and_project["project_id"],
+        token=token,
+        vlab_project_url=settings.virtual_lab.get_project_url,
+    )
+    return vlab_and_project
 
 
 def get_agents_routine(
@@ -162,3 +216,28 @@ def get_agents_routine(
 ) -> AgentsRoutine:
     """Get the AgentRoutine client."""
     return AgentsRoutine(openai)
+
+
+def get_starting_agent(
+    _: Annotated[None, Depends(get_vlab_and_project)],
+    settings: Annotated[Settings, Depends(get_settings)],
+) -> Agent:
+    """Get the starting agent."""
+    logger.info(f"Loading model {settings.openai.model}.")
+    agent = Agent(
+        name="Agent",
+        instructions="""You are a helpful assistant helping scientists with neuro-scientific questions.
+                You must always specify in your answers from which brain regions the information is extracted.
+                Do no blindly repeat the brain region requested by the user, use the output of the tools instead.""",
+        tools=[PrintAccountDetailsTool],
+        model=settings.openai.model,
+    )
+    return agent
+
+
+def get_context_variables(
+    settings: Annotated[Settings, Depends(get_settings)],
+    starting_agent: Annotated[Agent, Depends(get_starting_agent)],
+) -> dict[str, Any]:
+    """Get the global context variables to feed the tool's metadata."""
+    return {"user_id": 1234, "starting_agent": starting_agent}
--- swarm_copy/app/routers/threads.py
+++ swarm_copy/app/routers/threads.py
@@ -5,13 +5,22 @@ import logging
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from httpx import AsyncClient
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from neuroagent.app.app_utils import validate_project
+from neuroagent.app.config import Settings
 from swarm_copy.app.database.db_utils import get_thread
 from swarm_copy.app.database.schemas import MessagesRead, ThreadsRead, ThreadUpdate
 from swarm_copy.app.database.sql_schemas import Entity, Messages, Threads
-from swarm_copy.app.dependencies import get_session, get_user_id
+from swarm_copy.app.dependencies import (
+    get_httpx_client,
+    get_kg_token,
+    get_session,
+    get_settings,
+    get_user_id,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -20,11 +29,24 @@ router = APIRouter(prefix="/threads", tags=["Threads' CRUD"])
 
 @router.post("/")
 async def create_thread(
+    httpx_client: Annotated[AsyncClient, Depends(get_httpx_client)],
+    settings: Annotated[Settings, Depends(get_settings)],
+    token: Annotated[str, Depends(get_kg_token)],
+    virtual_lab_id: str,
+    project_id: str,
     session: Annotated[AsyncSession, Depends(get_session)],
     user_id: Annotated[str, Depends(get_user_id)],
     title: str = "New chat",
 ) -> ThreadsRead:
     """Create thread."""
+    # We first need to check if the combination thread/vlab/project is valid
+    await validate_project(
+        httpx_client=httpx_client,
+        vlab_id=virtual_lab_id,
+        project_id=project_id,
+        token=token,
+        vlab_project_url=settings.virtual_lab.get_project_url,
+    )
     new_thread = Threads(user_id=user_id, title=title)
     session.add(new_thread)
     await session.commit()
